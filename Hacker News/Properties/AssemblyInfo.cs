7322e2872b7df432dc82a866afc4e469d97b59ff
Joel Franusic <joel@franusic.com>
2010-11-28 21:36:44 -0800

    s/Hacker_News/Hacker News/g

--- Hacker News/Properties/AssemblyInfo.cs
+++ Hacker News/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hacker_News")]
+[assembly: AssemblyTitle("Hacker News")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Hacker_News")]
+[assembly: AssemblyProduct("Hacker News")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
